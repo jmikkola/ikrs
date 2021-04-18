1da0cdc063785e2c551c30d0dedd588e074712a0
Jeremy Mikkola <jeremy@jeremymikkola.com>
2021-04-17 21:56:52 -0400

    Start enforcing package naming

--- src/compiler.rs
+++ src/compiler.rs
@@ -75,7 +75,7 @@ pub fn compile(paths: Vec<String>, tokenize_only: bool) -> io::Result<()> {
 
 #[derive(Debug)]
 struct CompileJob {
-    packaged_files: Vec<Package>,
+    packages: Vec<Package>,
 }
 
 #[derive(Debug)]
@@ -96,14 +96,75 @@ impl Package {
 
     fn add_file_path(&mut self, filename: String) {
         self.file_paths.push(filename.clone());
-        self.syntaxes.push(ast::Syntax::new(filename));
+    }
+
+    fn parse_files(&mut self, tokenize_only: bool) -> io::Result<()> {
+        assert!(self.syntaxes.is_empty());
+        for file_path in self.file_paths.iter() {
+            let contents = read_path(file_path)?;
+
+            let tokens = tokenize(contents.as_str());
+            if tokens.has_unknown() {
+                // TODO: Handle errors in one file and continue to the next
+                eprintln!("cannot parse {}, found unknown tokens", file_path);
+                let err = io::Error::new(io::ErrorKind::Other, "error");
+                return Err(err);
+            }
+
+            if tokenize_only {
+                continue;
+            }
+
+            let syntax = parse(file_path.clone(), &tokens);
+            if syntax.has_errors() {
+                // TODO: Handle errors in one file and continue to the next
+                for e in syntax.errors.iter() {
+                    eprintln!("{}", e);
+                }
+                let err = io::Error::new(io::ErrorKind::Other, "error");
+                return Err(err);
+            }
+
+            self.syntaxes.push(syntax);
+        }
+        Ok(())
+    }
+
+    fn check_declared_names(&self) -> io::Result<()> {
+        let allowed_to_skip_package_decl = self.package_name == "main";
+
+        for syntax in self.syntaxes.iter() {
+            let first_decl = if let Some(decl) = syntax.declarations.get(0) {
+                decl
+            } else {
+                // ignore empty files
+                continue
+            };
+
+            if let ast::Declaration::PackageDecl(name) = first_decl {
+                if *name != self.package_name {
+                    eprintln!(
+                        "file {} should declare package {} but declares package {} instead",
+                        syntax.filename, self.package_name, name);
+                    let err = io::Error::new(io::ErrorKind::Other, "error");
+                    return Err(err);
+                }
+            } else if !allowed_to_skip_package_decl {
+                eprintln!(
+                    "file {} should declare package {} but doesn't",
+                    syntax.filename, self.package_name);
+                let err = io::Error::new(io::ErrorKind::Other, "error");
+                return Err(err);
+            }
+        }
+        Ok(())
     }
 }
 
 impl CompileJob {
     fn new() -> Self {
         CompileJob{
-            packaged_files: vec![],
+            packages: vec![],
         }
     }
 
@@ -119,7 +180,7 @@ impl CompileJob {
     }
 
     fn add_file_to_package(&mut self, package_path: String, filename: &String) {
-        for pck in self.packaged_files.iter_mut() {
+        for pck in self.packages.iter_mut() {
             if pck.package_name == package_path {
                 pck.file_paths.push(filename.clone());
                 return;
@@ -128,15 +189,20 @@ impl CompileJob {
 
         let mut pkg = Package::new(package_path);
         pkg.add_file_path(filename.clone());
-        self.packaged_files.push(pkg);
+        self.packages.push(pkg);
     }
 
     fn parse_files(&mut self, tokenize_only: bool) -> io::Result<()> {
-        // TODO
+        for pkg in self.packages.iter_mut() {
+            pkg.parse_files(tokenize_only)?;
+        }
         Ok(())
     }
 
-    fn check_declared_names(&mut self) -> io::Result<()> {
+    fn check_declared_names(&self) -> io::Result<()> {
+        for pkg in self.packages.iter() {
+            pkg.check_declared_names()?;
+        }
         Ok(())
     }
 
@@ -149,6 +215,7 @@ impl CompileJob {
     }
 }
 
+// TODO: Support finding base paths to the project (e.g. strip off the passed in directory)
 fn get_package_path(path: &String) -> String {
     let mut parts: Vec<String> = path::Path::new(path).components()
         .filter_map(|component| {
--- src/compiler/test.rs
+++ src/compiler/test.rs
@@ -1,7 +1,7 @@
 use super::*;
 
 fn get_package<'a>(gp: &'a CompileJob, name: &str) -> Option<&'a Package> {
-    for pf in gp.packaged_files.iter() {
+    for pf in gp.packages.iter() {
         if pf.package_name == name {
             return Some(pf);
         }
@@ -36,7 +36,7 @@ fn test_grouping_files() {
 
     let result = CompileJob::gather_files(&paths).unwrap();
 
-    assert!(result.packaged_files.len() == 3);
+    assert!(result.packages.len() == 3);
 
     let main_pkg = get_package(&result, "main").unwrap();
     assert!(main_pkg.file_paths.contains(&"main.ik".to_string()));
