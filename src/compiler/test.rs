1e12d29bed3e5a9468a195a885f03dfbfcaa3c0c
Jeremy Mikkola <jeremy@jeremymikkola.com>
2021-04-24 13:54:22 -0400

    Add multi-file tests

--- src/compiler/test.rs
+++ src/compiler/test.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use super::*;
 
 use tempdir;
@@ -56,23 +58,125 @@ fn test_grouping_files() {
 
 }
 
+fn setup_test_dir(name_and_contents: Vec<(&str, &str)>)
+                  -> io::Result<(tempdir::TempDir, String, Vec<String>)> {
+    let tmp_dir = tempdir::TempDir::new("ikrs-tests")?;
+    let base_dir = path_to_string(tmp_dir.path());
+
+    let mut file_paths = Vec::new();
+    for (name, content) in name_and_contents {
+        let file_path = tmp_dir.path().join(name);
+        ensure_parent_exists(&file_path)?;
+
+        let file_path_str = path_to_string(&file_path);
+
+        let mut file = File::create(file_path)?;
+        write!(file, "{}", content)?;
+
+        file_paths.push(file_path_str);
+    }
+
+    Ok((tmp_dir, base_dir, file_paths))
+}
+
+fn path_to_string(path: &Path) -> String {
+    path.to_str().unwrap().to_string()
+}
+
+fn ensure_parent_exists(path: &Path) -> io::Result<()> {
+    if let Some(parent) = path.parent() {
+        ensure_parent_exists(&parent)?;
+        if !parent.exists() {
+            std::fs::create_dir(parent)?;
+        }
+    }
+    Ok(())
+}
+
 #[test]
 fn test_building_single_main_file() {
     let main = r#"
 fn main():
   print("hello world")
 "#;
+    let setup = vec![("main.ik", main)];
+    let (_tmp_dir, base_path, file_paths) = setup_test_dir(setup).unwrap();
 
-    let tmp_dir = tempdir::TempDir::new("ikrs-tests").unwrap();
-    let main_path = tmp_dir.path().join("main.ik");
+    let result = compile(file_paths, &base_path, false);
+    assert!(result.is_ok(), "{:?}", result);
+}
 
-    let base_path = tmp_dir.path().to_str().unwrap().to_string();
-    let main_str = main_path.to_str().unwrap().to_string();
-    {
-        let mut tmp_file = File::create(main_path).unwrap();
-        write!(tmp_file, "{}", main).unwrap();
-    }
+#[test]
+fn test_building_multi_module_project() {
+    let main = r#"
+package main
 
-    let result = compile(vec![main_str], &base_path, false);
-    assert!(result.is_ok());
+fn main():
+  say_hello()
+"#;
+
+    let hello = r#"
+import message
+
+fn say_hello():
+  print(message.hello_message())
+"#;
+
+    let message = r#"
+package message
+
+fn hello_message():
+  return "Hello"
+"#;
+
+    let setup = vec![
+        ("main.ik", main),
+        ("hello.ik", hello),
+        ("message/message.ik", message),
+    ];
+    let (_tmp_dir, base_path, file_paths) = setup_test_dir(setup).unwrap();
+
+    let result = compile(file_paths, &base_path, false);
+    assert!(result.is_ok(), "{:?}", result);
+}
+
+#[test]
+fn test_fails_on_import_cycle() {
+    let main = r#"
+package main
+
+import a
+
+fn main():
+  a.f()
+"#;
+
+    let a = r#"
+package a
+
+import b
+
+fn f():
+  b.g()
+"#;
+
+    let b = r#"
+package b
+
+import a
+
+fn g():
+  a.f()
+"#;
+
+    let setup = vec![
+        ("main.ik", main),
+        ("a/a.ik", a),
+        ("b/b.ik", b),
+    ];
+    let (_tmp_dir, base_path, file_paths) = setup_test_dir(setup).unwrap();
+
+    let result = compile(file_paths, &base_path, false);
+    // TODO: Make an assertion about which error it is once that's possible
+    assert!(result.is_err(), "{:?}", result);
 }
