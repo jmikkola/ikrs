ae2fcbed1b2223e07f0592cd15f723bc225b2310
Jeremy Mikkola <jeremy@jeremymikkola.com>
2021-04-25 09:50:26 -0400

    Add utility for indexed values

--- src/inference/mod.rs
+++ src/inference/mod.rs
@@ -2,19 +2,16 @@
 
 use std::collections::HashMap;
 
+use super::util::Indexed;
+
 #[cfg(test)]
 mod test;
 
 struct Inference {
-    kinds: Vec<Kind>,
-    types: Vec<Type>,
-    strings: Vec<String>,
-
-    kind_to_ref: HashMap<Kind, KindRef>,
-    type_to_ref: HashMap<Type, TypeRef>,
-    string_to_ref: HashMap<String, SRef>,
+    kinds: Indexed<Kind>,
+    types: Indexed<Type>,
+    strings: Indexed<String>,
 }
-
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub struct KindRef(usize);
@@ -28,25 +25,20 @@ pub struct SRef(usize);
 
 impl Inference {
     fn new() -> Self {
-        let mut kinds = Vec::new();
-        let mut kind_to_ref = HashMap::new();
+        let mut kinds = Indexed::new();
         // Reserve the zeroth ref for Star
-        kinds.push(Kind::Star);
-        kind_to_ref.insert(Kind::Star, KindRef(0));
-
+        let r = kinds.store(Kind::Star);
+        debug_assert!(r == 0);
 
         Inference{
             kinds: kinds,
-            types: Vec::new(),
-            strings: Vec::new(),
-            kind_to_ref: kind_to_ref,
-            type_to_ref: HashMap::new(),
-            string_to_ref: HashMap::new(),
+            types: Indexed::new(),
+            strings: Indexed::new(),
         }
     }
 
     pub fn star_kind(&self) -> KindRef {
-        debug_assert!(self.kind_to_ref.contains_key(&Kind::Star));
+        debug_assert!(self.kinds.get(0) == &Kind::Star);
         KindRef(0)
     }
 
@@ -55,54 +47,27 @@ impl Inference {
         // they should be values returned by either this method or by
         // `star_kind()`.
         let resulting_kind = Kind::App(left, right);
-        // Which means that if this kind is already defined, it will appear in
-        // this map with the exact same left and right references
-        if let Some(kref) = self.kind_to_ref.get(&resulting_kind) {
-            return *kref;
-        }
-
-        let next_ref = KindRef(self.kinds.len());
-        self.kinds.push(resulting_kind.clone());
-        self.kind_to_ref.insert(resulting_kind, next_ref);
-
-        next_ref
-    }
-
-    // TODO: This structure could be extracted to a shared type (likely useful in parsing, too)
-    pub fn save_string(&mut self, s: &String) -> SRef {
-        if let Some(sref) = self.string_to_ref.get(s) {
-            return *sref;
-        }
-
-        let next_ref = SRef(self.strings.len());
-        self.strings.push(s.clone());
-        self.string_to_ref.insert(s.clone(), next_ref);
-
-        next_ref
-    }
-
-    pub fn save_type(&mut self, t: Type) -> TypeRef {
-        if let Some(tref) = self.type_to_ref.get(&t) {
-            return *tref;
-        }
-
-        let next_ref = TypeRef(self.types.len());
-        self.types.push(t.clone());
-        self.type_to_ref.insert(t, next_ref);
-
-        next_ref
+        KindRef(self.kinds.store(resulting_kind))
     }
 
     pub fn get_kind(&self, KindRef(r): KindRef) -> &Kind {
-        &self.kinds[r]
+        self.kinds.get(r)
     }
 
-    pub fn get_type(&self, TypeRef(r): TypeRef) -> &Type {
-        &self.types[r]
+    pub fn save_string(&mut self, s: String) -> SRef {
+        SRef(self.strings.store(s))
     }
 
     pub fn get_string(&self, TypeRef(r): TypeRef) -> &String {
-        &self.strings[r]
+        self.strings.get(r)
+    }
+
+    pub fn save_type(&mut self, t: Type) -> TypeRef {
+        TypeRef(self.types.store(t))
+    }
+
+    pub fn get_type(&self, TypeRef(r): TypeRef) -> &Type {
+        self.types.get(r)
     }
 }
 
